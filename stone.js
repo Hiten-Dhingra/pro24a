eb22fba2c7ca490ae9656746d8a8e06dac263132
Hiten-Dhingra <81410208+Hiten-Dhingra@users.noreply.github.com>
2021-05-24 13:18:23 +0530

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,25 @@
+class Stone {
+    constructor(x, y) {
+      var options = {
+        'density':12,
+        'friction': 0.9,
+        'restitution':0.8
+      };
+      this.body = Bodies.rectangle(x, y, 50, 50, options);
+      this.width = 150;
+      this.height = 150;
+      World.add(world, this.body);
+    };
+    display(){
+       var pos =this.body.position;
+        var angle = this.body.angle;
+         push(); translate(pos.x, pos.y); 
+         rotate(angle);
+          rectMode(CENTER);
+           strokeWeight(4); 
+           stroke("black");
+            fill("black");
+             rect(0, 0, this.width, this.height);
+              pop();
+   };
+  }
